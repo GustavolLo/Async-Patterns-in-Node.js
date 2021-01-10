044bbdbb1a780de20e2790facd51ba0bb46c4b12
GustavolLo <goodluke111931@gmail.com>
2021-01-10 15:14:12 +0100

    Refactoring clothing using a Promise.

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -5,25 +5,26 @@ const router = express.Router();
 
 /* GET all clothing */
 router.route("/").get(function (req, res) {
-  getClothingData((err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
+  getClothingData()
+    .then((data) => {
       console.log("Returning clothing data");
       res.send(data);
-    }
-  });
+    })
+    .catch((err) => res.status(500).send(err))
+    .finally(() => console.log("All done processing promise"));
   console.log("Ready to work again");
 });
 
-function getClothingData(callback) {
-  fs.readFile(datafile, "utf8", (err, data) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      let clothingData = JSON.parse(data);
-      callback(null, clothingData);
-    }
+function getClothingData() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(datafile, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let clothingData = JSON.parse(data);
+        resolve(clothingData);
+      }
+    });
   });
 }
 

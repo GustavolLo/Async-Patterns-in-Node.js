7479357c72e84c5fd5a61ba8fe3db23ce1a60573
GustavolLo <goodluke111931@gmail.com>
2021-01-10 16:16:21 +0100

    Refactoring clothing using async/await.

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,31 +1,42 @@
 const express = require("express");
 const fs = require("fs");
+const fsPromises = require("fs").promises;
 const datafile = "server/data/clothing.json";
 const router = express.Router();
 
 /* GET all clothing */
-router.route("/").get(function (req, res) {
-  getClothingData()
-    .then((data) => {
-      console.log("Returning clothing data");
-      res.send(data);
-    })
-    .catch((err) => res.status(500).send(err))
-    .finally(() => console.log("All done processing promise"));
-  console.log("Ready to work again");
+router.route("/").get(async function (req, res) {
+  try {
+    let data = await getClothingData();
+    console.log("Returning async data");
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+  // getClothingData()
+  //   .then((data) => {
+  //     console.log("Returning clothing data");
+  //     res.send(data);
+  //   })
+  //   .catch((err) => res.status(500).send(err))
+  //   .finally(() => console.log("All done processing promise"));
+  // console.log("Ready to work again");
 });
 
-function getClothingData() {
-  return new Promise((resolve, reject) => {
-    fs.readFile(datafile, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        let clothingData = JSON.parse(data);
-        resolve(clothingData);
-      }
-    });
-  });
+async function getClothingData() {
+  const rawData = await fsPromises.readFile(datafile, "utf8");
+  const clothingData = JSON.parse(rawData);
+  return clothingData;
+  // return new Promise((resolve, reject) => {
+  //   fs.readFile(datafile, "utf8", (err, data) => {
+  //     if (err) {
+  //       reject(err);
+  //     } else {
+  //       let clothingData = JSON.parse(data);
+  //       resolve(clothingData);
+  //     }
+  //   });
+  // });
 }
 
 module.exports = router;
